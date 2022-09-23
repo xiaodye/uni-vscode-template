367bd12d7c9c03fd88f7580339ae6f672c0bef6a
lansen <bluewoods.lan@gmail.com>
2022-09-23 10:54:35 +0800

    feat:移除无用的配置

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,4 @@
 module.exports = {
-  pages: {
-    index: {
-      entry: "src/main.js",
-    },
-  },
   lintOnSave: false,
-  configureWebpack: {
-    resolve: {
-      alias: {
-        static: "@/static",
-      },
-    },
-  },
   transpileDependencies: ["uview-ui"],
 }
